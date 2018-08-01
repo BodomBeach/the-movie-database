e39c0863dc7059be7d569966681b937d1479ae55
Nicolas <nicolas.bertin0@gmail.com>
2018-08-01 13:47:29 +0200

    added movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Movie.all.destroy
+
+Movie.create(title: 'Le Grand Bleu', release_year: 1988, director: 'Luc Besson')
+Movie.create(title: 'Le 5ème élement', release_year: 1997, director: 'Luc Besson')
+Movie.create(title: 'Les visiteurs', release_year: 1993, director: 'Jean-Marie Poiré')
+Movie.create(title: 'Avatar', release_year: 2009, director: 'James Cameron')
+Movie.create(title: 'Retour vers le futur', release_year: 1985, director: 'Robert Zemeckis')
